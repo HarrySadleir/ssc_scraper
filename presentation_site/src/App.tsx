9f432effe967ff5dadbfb6f618e55dfb551086bd
HarrySadleir <hereisharrys@gmail.com>
2022-11-21 09:41:38 -0800

    Fix sizing policy and added sidebar

--- presentation_site/src/App.tsx
+++ presentation_site/src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
+import Sidenav from "./Sidenav";
 
 function App() {
   return (
     <>
-      <div>
-        <ul>
-          <li><a href='/ba_specialties.html'>Bachelor of Arts</a></li>
-          <li><a href='/all_bachelors.html'>All 'Bachelors' Specializations</a></li>
-          <li><a href='/all_specialties.html'>All UBC Specializations</a></li>
-        </ul>
-        <hr />
-      </div >
+      <Sidenav/>
     </>
   );
 }
---
+++ presentation_site/src/Sidenav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidenav.css";
+
+function Sidenav(): React.ReactElement {
+    return (<>
+        <div className="sidenav">
+            <a href='/ba_specialties.html'>BA Specializations</a>
+            <a href='/all_bachelors.html'>'Bachelors' Specializations</a>
+            <a href='/all_specialties.html'>All Specializations</a>
+        </div>
+    </>)
+}
+
+export default Sidenav;
