01f24692a36939b75bc362a1d361e20178fa647a
HarrySadleir <hereisharrys@gmail.com>
2022-11-20 20:42:05 -0500

    Initial site work

---
+++ presentation_site/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ul>
+          <li><a href='/ba_specialties.html'>Bachelor of Arts</a></li>
+          <li><a href='/all_bachelors.html'>All 'Bachelors' Specializations</a></li>
+          <li><a href='/all_specialties.html'>All UBC Specializations</a></li>
+        </ul>
+        <hr />
+      </div >
+    </>
+  );
+}
+
+export default App;
