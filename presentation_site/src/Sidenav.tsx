c6a708b0fd9d5c1565716e73b674ab14c4b4510e
HarrySadleir <hereisharrys@gmail.com>
2022-11-27 13:47:13 -0800

    Style change and use Link where possible

--- presentation_site/src/Sidenav.tsx
+++ presentation_site/src/Sidenav.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Sidenav.css";
+import { Link } from "react-router-dom";
 
 function Sidenav(): React.ReactElement {
     return (<>
         <div className="sidenav">
-            <a href='/'>Home</a> 
-            <a href='/facultiesTimeline'>Faculties Over Time</a> 
-            <a href='/comparativeCharts'>Comparative Faculty Charts</a>
-            <a href='/enrolmentTimeline'>Enrolment Over Time</a>
+            <Link to='/'>Home</Link> 
+            <Link to='/facultiesTimeline'>Faculties Over Time</Link> 
+            <Link to='/comparativeCharts'>Comparative Faculty Charts</Link>
+            <Link to='/enrolmentTimeline'>Enrolment Over Time</Link>
             <a href='/ba_specialties.html'>BA Specializations</a>
             <a href='/all_bachelors.html'>Bachelor's Specializations</a>
             <a href='/all_specialties.html'>All Specializations</a>
